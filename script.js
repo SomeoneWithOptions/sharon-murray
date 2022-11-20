c209f16298663005065e2b393c1b661ed68c0ae8
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-20 09:44:06 -0500

    starting animation for images

---
+++ script.js
@@ -0,0 +1,9 @@
+function removeStartigImageAnimations() {
+    let images = document.querySelectorAll('img');
+    [...images].map(image => { 
+        image.classList.remove('startAnimation')
+        image.style.opacity = 1;
+    })
+}
+
+setTimeout(removeStartigImageAnimations, 2500);
