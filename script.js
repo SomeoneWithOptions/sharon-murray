713cab5296dfe5add3449cf03f2cf5373a6b9410
SomeoneWithOptions <andrescastesol@gmail.com>
2022-12-10 14:09:06 -0500

    removed camsoda and chatubate links

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function removeStartigImageAnimations() {
-  let images = document.querySelectorAll("img");
-  [...images].map((image) => {
-    image.classList.remove("startAnimation");
-    image.style.opacity = 1;
-  });
-}
+// function removeStartigImageAnimations() {
+//   let images = document.querySelectorAll("img");
+//   [...images].map((image) => {
+//     image.classList.remove("startAnimation");
+//     image.style.opacity = 1;
+//   });
+// }
 
-setTimeout(removeStartigImageAnimations, 2500);
+// setTimeout(removeStartigImageAnimations, 2500);
